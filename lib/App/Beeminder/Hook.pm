90ab38d9f9b6955ae75727817be3788f877d43ed
Jonathan "Duke" Leto <jonathan@leto.net>
2012-06-30 13:29:15 -0700

    Decode the post-receive json

--- lib/App/Beeminder/Hook.pm
+++ lib/App/Beeminder/Hook.pm
@@ -8,8 +8,10 @@ our $VERSION = '0.1';
 
 # inspired by https://github.com/solgenomics/sgn-devtools/blob/master/git/github_post_receive
 
+# This will eventually use WWW::Beeminder when that is ready, but for now, curl saves the day
 any '/hook' => sub {
-    my $payload = param('payload');
+    my $p = param('payload');
+    $p = JSON::Any->new->decode( $p );
 };
 
 true;
